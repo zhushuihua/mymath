13f6b061df2903c42817786919e914b6dcd7a138
Joseph <null>
2018-11-20 11:33:36 +0800

    Initial Commit

---
+++ My Maths/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Maths
+//
+//  Created by Shuihua Zhu on 20/11/18.
+//  Copyright © 2018 UMA Mental Arithmetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
